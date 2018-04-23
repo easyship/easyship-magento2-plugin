9322b60e59c5989b50fdcba9411628a8aaa29fdc
Holubiatnikova Anna <holubiatnikova.anna@gmail.com>
2018-04-23 15:08:02 +0300

    Easyship Carrier: update trigger

---
+++ app/code/Goeasyship/Shipping/view/frontend/web/js/model/shipping-rates-validation-rules.js
@@ -0,0 +1,39 @@
+/**
+ * Easyship.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Easyship.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Goeasyship
+ * @package     Goeasyship_Shipping
+ * @copyright   Copyright (c) 2018 Easyship (https://www.easyship.com/)
+ * @license     https://www.apache.org/licenses/LICENSE-2.0
+ */
+
+define([], function () {
+    'use strict';
+
+    return {
+        /**
+         * @return {Object}
+         */
+        getRules: function () {
+            return {
+                'country_id': {
+                    'required': true
+                },
+                'postcode': {
+                    'required': true
+                }
+            };
+        }
+    };
+});
---
+++ app/code/Goeasyship/Shipping/view/frontend/web/js/view/shipping-rates-validation.js
@@ -0,0 +1,40 @@
+/**
+ * Easyship.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Easyship.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Goeasyship
+ * @package     Goeasyship_Shipping
+ * @copyright   Copyright (c) 2018 Easyship (https://www.easyship.com/)
+ * @license     https://www.apache.org/licenses/LICENSE-2.0
+ */
+
+define([
+    'uiComponent',
+    'Magento_Checkout/js/model/shipping-rates-validator',
+    'Magento_Checkout/js/model/shipping-rates-validation-rules',
+    '../model/shipping-rates-validator',
+    '../model/shipping-rates-validation-rules'
+], function (
+    Component,
+    defaultShippingRatesValidator,
+    defaultShippingRatesValidationRules,
+    easyshipShippingRatesValidator,
+    easyshipShippingRatesValidationRules
+) {
+    'use strict';
+
+    defaultShippingRatesValidator.registerValidator('easyship', easyshipShippingRatesValidator);
+    defaultShippingRatesValidationRules.registerRules('easyship', easyshipShippingRatesValidationRules);
+
+    return Component;
+});
